308e9cccf690025f0ae83b500b6aab9fb4d9c712
Miguel Segura <mseguraosiann@gmail.com>
2025-10-08 17:46:45 -0600

    rutas con nombre

--- routes/web.php
+++ routes/web.php
@@ -12,14 +12,14 @@ Route::get('/', [HomeController::class, 'index']);
 Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
 
 //aqui se crea un nuevo post y se guarda en la base de datos
-Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
+Route::get('/nuevo_artuculo/create', [PostController::class, 'create'])->name('posts.create');
 Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
 
 //aqui se muestra un post en particular
 Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show');
 
 //aqui se edita un post en particular y se actualiza en la base de datos
-Route::get('/posts/{post}/edit', [PostController::class, 'edit'])->name('posts.edit');
+Route::get('/editar_articulo/{post}/edit', [PostController::class, 'edit'])->name('posts.edit');
 Route::put('/posts/{post}', [PostController::class, 'update'])->name('posts.update');
 
 //aqui se elimina un post en particular
